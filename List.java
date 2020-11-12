612b668898b67691ea3c22e38d62bd1effec1f3f
Ryota Abe <ryotaabe@RyotanoMacBook-Pro.local>
2020-11-12 12:16:15 +0900

    first commit

---
+++ List.java
@@ -0,0 +1,13 @@
+package dataStructure;
+
+public class List {
+		public static void main(String[] args) {
+		String[] colors = new String[3];
+		colors[0] = "Blue";
+		colors[1] = "Yellow";
+		colors[2] = "Red";
+
+		System.out.println(colors[0]);
+		//配列を使ったこのコードだと要素追加ができない
+	}
+}
---
+++ List2.java
@@ -0,0 +1,19 @@
+package dataStructure;
+import java.util.ArrayList;
+public class List2 {
+	public static void main(String[] args) {
+	ArrayList<String> colors = new ArrayList<String>();
+	colors.add(0,"Brue");
+	colors.add(1,"Yellow");
+	colors.add(2,"Red");
+//	colors.add(1,"Green"); //ここで第1要素(2番目)にGreenが入る
+
+	for(String c : colors) {
+
+	System.out.println(c);
+
+		}
+
+	}
+
+}
