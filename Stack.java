9920452ba7560bc04d6e9efe5ffe06663ee58e3c
Ryota Abe <ryotaabe@RyotanoMacBook-Pro.local>
2020-11-13 11:56:19 +0900

    簡単なスタック

--- Stack.java
+++ Stack.java
@@ -1,9 +1,11 @@
+//コレクションを利用した簡単なスタック
 package dataStructure;
 import java.util.ArrayList;
+import java.util.List;
 public class Stack {
 
 	public static void main(String[] args) {
-		ArrayList<String> colors = new ArrayList<String>();
+		List<String> colors = new ArrayList<String>();
 		String c = null;
 		colors.add(0,"Blue");
 		colors.add(1,"Green");
