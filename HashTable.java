3aacfb007e28ede8e673d74c16fcabbf8f471dc0
Ryota Abe <ryotaabe@RyotanoMacBook-Pro.local>
2020-11-13 12:40:31 +0900

    first commit

---
+++ HashTable.java
@@ -0,0 +1,27 @@
+package dataStructure;
+import java.util.HashMap;
+import java.util.Map;
+public class HashTable {
+
+	public static void main(String[] args) {
+		Map<String,String> names = new HashMap<String, String>();
+		names.put("Joe","M");
+		names.put("Sue","F");
+		names.put("Dan","M");
+		names.put("Nell","F");
+		names.put("Ally","F");
+		names.put("Bob","M");
+		//Allyを探す
+		int i = 0;
+		String s = null;
+		for(; i < names.size(); i++) {
+			if(names.get("Ally") == "Ally") {
+				s = names.get("Ally");
+				break;
+			}
+		}
+		System.out.println("Allyの性別は"+s);
+		System.out.println("Allyは"+i+"番目");
+	}
+
+}
